4e6b447c2740f45c5b815fdc616223685eb51b70
Birzhan Serik <birzhans14@gmail.com>
2020-10-08 03:28:15 +0600

    change tests_by_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   def tests_by_level(level)
     Test.where(user_id: self.id, level: level)
+    Tests.joins("JOIN tests_users ON tests.id = tests_users.id")
+    .where(tests_users: {user_id: self.id}, tests: {level: level})
   end
 end
