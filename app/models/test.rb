caf146fab986d192170a376335223f5ca67acc0e
Birzhan Serik <49227586+birzhans@users.noreply.github.com>
2020-10-12 16:51:43 +0600

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,14 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  has_many :questions, dependent: :destroy
+  has_many :tests_users, dependent: :destroy
+  has_many :users, through: :tests_users
+  
   def self.by_category(category_name)
      joins(:category)
        .where("categories.title = ?", category_name)
        .order(title: :desc)
        .pluck(:id)
   end
-
-  belongs_to :category
-
-  has_many :questions, dependent: :destroy
-  has_many :tests_users, dependent: :destroy
-  has_many :users, through: :tests_users
 end
