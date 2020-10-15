81f2097e830dba8e3adef49b90ef624c12fb0540
Birzhan Serik <birzhans14@gmail.com>
2020-10-15 10:57:42 +0600

    add correct column to answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_062336) do
+ActiveRecord::Schema.define(version: 2020_10_15_045319) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "question_id", null: false
+    t.boolean "correct", default: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
