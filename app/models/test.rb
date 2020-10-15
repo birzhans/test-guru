eeb882273e84fae23e8105e518e4ab56ad93aa7c
Birzhan Serik <birzhans14@gmail.com>
2020-10-15 11:25:43 +0600

    add scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,19 @@
 class Test < ApplicationRecord
-  def self.by_category(category_name)
+  belongs_to :category
+
+  has_many :questions, dependent: :destroy
+  has_many :tests_users, dependent: :destroy
+  has_many :users, through: :tests_users
+
+  scope :by_category, -> (category_name) do
      joins(:category)
        .where("categories.title = ?", category_name)
        .order(title: :desc)
        .pluck(:id)
   end
 
-  belongs_to :category
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :advanced, -> { where(level: 5..Float::INFINITY) }
 
-  has_many :questions, dependent: :destroy
-  has_many :tests_users, dependent: :destroy
-  has_many :users, through: :tests_users
 end
