6e07a1fe23cc0624f33373fb8b2fb2ab4fd0e768
Birzhan Serik <birzhans14@gmail.com>
2020-10-24 16:51:06 +0600

    test create & update controllers and views

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,9 +1,47 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update]
   def index
     @tests = Test.all
   end
 
   def show
+    @test
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @test
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_test
     @test = Test.find(params[:id])
   end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
 end
