73ea2e99ce104b49a932775bc1b4161de63f6cec
Birzhan Serik <birzhans14@gmail.com>
2020-10-24 18:38:59 +0600

    add helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def form_header(question)
+    if @question.new_record?
+      "Create new #{@test.title} question"
+    else
+      "Edit #{@test.title} question"
+    end
+  end
 end
