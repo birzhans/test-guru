d25fb3428121a363f0fcabfdabead367655a52c4
Birzhan Serik <birzhans14@gmail.com>
2020-11-02 11:48:48 +0600

    rename password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_094950) do
+ActiveRecord::Schema.define(version: 2020_11_02_054318) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2020_10_28_094950) do
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
