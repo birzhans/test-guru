8606fe88ff8fbb2d22cd81607686754698487961
Birzhan Serik <birzhans14@gmail.com>
2020-11-03 00:24:35 +0600

    add session controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,5 @@
 class TestsController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_test, only: %i[destroy show edit update start]
   before_action :find_user, only: :start
 
