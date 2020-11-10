33ff442696d95e3bcdf7355b8664b1a3d5fcdff7
Birzhan Serik <birzhans14@gmail.com>
2020-11-10 12:59:45 +0600

    add admin resources

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,43 +1,8 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[destroy show edit update start]
+  before_action :find_test, only: :start
 
   def index
     @tests = Test.all
-  end
-
-  def show
-    @test
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.new(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @test
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -49,9 +14,5 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id)
   end
 end
