e12049fff35b002e1a0db56aa3d74e40462cc482
Birzhan Serik <birzhans14@gmail.com>
2020-11-14 22:55:12 +0600

    add devise locales

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module TestGuru
     config.time_zone = 'Almaty'
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     I18n.available_locales = [:en, :ru]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
