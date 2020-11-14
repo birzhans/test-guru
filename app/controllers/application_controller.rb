a3874db150216006a593f3cdb2752cceed28f5bd
Birzhan Serik <birzhans14@gmail.com>
2020-11-14 16:37:49 +0600

    set russian locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale
 
   def after_sign_in_path_for(user)
     flash[:notice] = "Hello, #{user.first_name}!"
-    if user.admin?
-      admin_tests_path
-    else
-      root_path
-    end
+    if user.admin? ? admin_tests_path : root_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale =  I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
