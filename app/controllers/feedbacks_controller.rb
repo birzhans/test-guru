4a1a853263bfc926cfa10f2c64a67df6fc142c93
Birzhan Serik <birzhans14@gmail.com>
2020-12-08 16:50:07 +0600

    add feedback previews

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,5 +1,4 @@
 class FeedbacksController < ApplicationController
-  before_action :find_test, only: :start
 
   def new
     @feedback = Feedback.new
@@ -9,6 +8,7 @@ class FeedbacksController < ApplicationController
     @feedback = Feedback.new(params.require(:feedback).permit(:message))
 
     if @feedback.save
+      FeedbackMailer.send_feedback(@feedback)
       redirect_to root_path, notice: t('.success')
     else
       render :new, notice: t('.failure')
